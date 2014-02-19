a81108a3075bf0848820539a9b16d7897aa7ad04
Chichok <Chichok1080@hotmail.com>
2014-02-18 16:51:45 -0800

    Player 1 dedication
    Player 1's jumps are dedicated and walks backwards in relation to player
    2

--- unity-project/Assets/scripts/BplayerControllerScript.cs
+++ unity-project/Assets/scripts/BplayerControllerScript.cs
@@ -5,6 +5,10 @@ public class BplayerControllerScript : MonoBehaviour
 {
 	public float maxspeed = 10f;
 	bool facingRight = true;
+	float Cposition;
+	private Transform Cwhere;
+
+	bool noControl1 = false; //set to true when an event takes away control from player
 
 	Animator anim;
 	bool grounded = false;
@@ -12,27 +16,64 @@ public class BplayerControllerScript : MonoBehaviour
 	float groundRadius = 0.1f;
 	public LayerMask whatIsGround;
 	public float jumpForce;
+	float move;
 
 	// Use this for initialization
 	void Start () 
 	{
 		anim = GetComponent<Animator> ();
+		Cwhere = GameObject.FindGameObjectWithTag("CPlayer2").transform;
+
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () 
 	{
-		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
+		//initializers for the frame
 		anim.SetBool ("Ground", grounded);
-
+		anim.SetFloat ("speed", move);
+		anim.SetBool ("facingRighty", facingRight);
 		anim.SetFloat ("vSpeed", rigidbody2D.velocity.y);
 
-		float move = Input.GetAxis ("Horizontal1");
+		//event is happening (hyper, being juggled, etc)
+		if (noControl1 == true) 
+		{
+			return;
+		}		 
 
-		anim.SetFloat("speed", Mathf.Abs(move));
+		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
+		if( grounded == false )
+		{
+			return;
+		}
+		else 
+		{
+			//checking if turning around is needed
+			Cposition = Cwhere.position.x;
+			if (Cposition > transform.position.x && facingRight == false) 
+			{
+				facingRight = false;
+				Flip ();
+			} 
+			else if (Cposition < transform.position.x && facingRight == true) 
+			{
+				facingRight = true;
+				Flip ();
+			}
 
-		rigidbody2D.velocity = new Vector2 (move * maxspeed, rigidbody2D.velocity.y);
-	
+
+
+						
+
+
+			//moving left and right
+			move = Input.GetAxis ("Horizontal1");
+
+
+
+			rigidbody2D.velocity = new Vector2 (move * maxspeed, rigidbody2D.velocity.y);
+
+						/*
 		if ( Input.GetAxis("Horizontal1") > 0 && !facingRight) 
 		{
 			Flip ();
@@ -41,15 +82,22 @@ public class BplayerControllerScript : MonoBehaviour
 		{
 			Flip ();
 		}
+		*/
+
+			//code to jump
+			if (grounded && (Input.GetAxis ("Jump1") < 0)) 
+			{
+				anim.SetBool ("Ground", false);
+				rigidbody2D.AddForce (new Vector2 (0, jumpForce));
+			}
+		}
 	}
+
+
 	//Input.GetKeyDown (KeyCode.Space))
 	void Update()
 	{
-		if ( grounded && ( Input.GetAxis("Jump1") < 0 ) ) 
-		{
-			anim.SetBool ("Ground", false);
-			rigidbody2D.AddForce( new Vector2(0,jumpForce));
-		}
+
 	}
 
 	void Flip()
