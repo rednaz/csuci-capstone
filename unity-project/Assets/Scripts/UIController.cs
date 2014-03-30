70cef2f6a2076bf05c82e105850aa12f9fa741dc
Evan Taylor <etaylor8086@gmail.com>
2014-03-30 00:54:04 -0700

    Cleanup on UI files and project data
    Old UI has been deleted from the project. This includes the
    horribly-named C# file used for it, GameController.cs.
    
    Renamed 'UnityFightScript.cs' to the more appropriate name,
    'UnityController.cs'

--- unity-project/Assets/Scripts/UIController.cs
+++ unity-project/Assets/Scripts/UIController.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using System;
 
-public class UIFightScript : MonoBehaviour
+public class UIController : MonoBehaviour
 {	
 	private float currentResX, currentResY;
 	private float bHealth, bMaxHealth, cHealth, cMaxHealth;
